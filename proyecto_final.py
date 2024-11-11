fcba81f4aa812269c0a8806e264b3e2bf208bc54
JDSP <santajosedavid@gmail.com>
2024-11-11 11:01:57 -0500

    Interfaz mejorada y capacidad de hacer reportes

--- proyecto_final.py
+++ proyecto_final.py
@@ -114,7 +114,6 @@ class Hotel:
      else:
         messagebox.showerror("Error", "Cliente no encontrado")
 
-
     def obtener_historial_reservas(self, id_cliente):
         historial = self.df_reservas[self.df_reservas["ID_Cliente"] == id_cliente]
         return historial
@@ -122,10 +121,15 @@ class Hotel:
     def reporte_ocupacion(self):
         ocupadas = self.df_reservas[self.df_reservas["Estado"] == "Activa"]
         fechas = ocupadas["Fecha_Inicio"]
-        plt.plot(fechas.value_counts().sort_index())
+        ocupacion_por_fecha = fechas.value_counts().sort_index()
+
+        # Graficar ocupación por fecha
+        ocupacion_por_fecha.plot(kind="bar", color="#4EAA03")
         plt.title("Ocupación por fecha")
         plt.xlabel("Fecha")
-        plt.ylabel("Ocupación")
+        plt.ylabel("Cantidad de habitaciones ocupadas")
+        plt.xticks(rotation=45)
+        plt.tight_layout()
         plt.show()
 
     def reporte_ingresos(self, fecha_inicio, fecha_fin):
@@ -139,10 +143,11 @@ class Hotel:
 
     def analisis_demanda(self):
         tipos = self.df_reservas.merge(self.df_habitaciones, on="ID_Habitacion")["Tipo"].value_counts()
-        tipos.plot(kind="bar")
+        tipos.plot(kind="bar", color="#FF9B42")
         plt.title("Demanda por tipo de habitación")
-        plt.xlabel("Tipo de Habitación")
-        plt.ylabel("Reservas")
+        plt.xlabel("Tipo de habitación")
+        plt.ylabel("Cantidad de reservas")
+        plt.tight_layout()
         plt.show()
 
 class HotelApp:
@@ -150,52 +155,92 @@ class HotelApp:
         self.hotel = Hotel()
         self.ventana = tk.Tk()
         self.ventana.title("Sistema de Reservas de Hotel")
+        self.ventana.geometry("1250x700")
+        self.ventana.resizable(0,0)
+        self.ventana.config(bg="#CAF0FB")
 
-        self.entry_id_habitacion = self.create_entry(self.ventana, "ID de la habitación:")
-        self.entry_tipo_habitacion = self.create_entry(self.ventana, "Tipo de habitación:")
-        self.entry_capacidad_habitacion = self.create_entry(self.ventana, "Capacidad de la habitación:")
-        self.entry_precio_habitacion = self.create_entry(self.ventana, "Precio por noche:")
-        self.entry_disponibilidad_habitacion = self.create_entry(self.ventana, "Disponibilidad (True/False):")
+    # FRAMES
+        self.frame_izquierdo = tk.Frame(self.ventana,bg="#D0EDFC")
+        self.frame_izquierdo.pack(side="left", padx=50, pady=20)
+
+        self.frame_derecho = tk.Frame(self.ventana,bg="#D0EDFC")
+        self.frame_derecho.pack(side="right", padx=50, pady=20)
+
+        self.frame_medio = tk.Frame(self.ventana,bg="#D0EDFC")
+        self.frame_medio.pack(side="top", padx=20, pady=40)
+
+        self.frame_bajo = tk.Frame(self.ventana,bg="#D0EDFC")
+        self.frame_bajo.pack(side="bottom", padx=5)
+
+    # LABELS
+        self.main_label = tk.Label(self.frame_medio,text="SISTEMA DE GESTION HOTELERA",font=('Arial',30),bg="#D0EDFC",fg="#3D0B60")
+        self.main_label.pack(pady=30)
+
+        self.label_left = tk.Label(self.frame_izquierdo, text="HABITACONES", font=("Arial"),bg="#D0EDFC",fg="#C20000")
+        self.label_left.pack(pady=5)
+
+        self.label_right = tk.Label(self.frame_derecho, text="CLIENTES", font=("Arial"),bg="#D0EDFC",fg="#C20000")
+        self.label_right.pack(pady=5)
+
+        self.label_down = tk.Label(self.frame_bajo, text="REPORTES", font=("Arial"),bg="#D0EDFC",fg="#C20000")
+        self.label_down.pack(pady=5)
+
+        self.label_up = tk.Label(self.frame_medio, text="RESERVAS", font=("Arial"),bg="#D0EDFC",fg="#C20000")
+        self.label_up.pack(pady=5)
+
+    # ENTRYS
+        self.entry_id_habitacion = self.create_entry(self.frame_izquierdo, "ID de la habitación:")
+        self.entry_tipo_habitacion = self.create_entry(self.frame_izquierdo, "Tipo de habitación:")
+        self.entry_capacidad_habitacion = self.create_entry(self.frame_izquierdo, "Capacidad de la habitación:")
+        self.entry_precio_habitacion = self.create_entry(self.frame_izquierdo, "Precio por noche:")
+        self.entry_disponibilidad_habitacion = self.create_entry(self.frame_izquierdo, "Disponibilidad (True/False):")
         
-        self.entry_id_reserva = self.create_entry(self.ventana, "ID de reserva:")
-        self.entry_id_cliente = self.create_entry(self.ventana, "ID de cliente:")
-        self.entry_nueva_fecha_inicio = self.create_entry(self.ventana, "Nueva fecha de inicio (YYYY-MM-DD):")
-        self.entry_nueva_fecha_fin = self.create_entry(self.ventana, "Nueva fecha de fin (YYYY-MM-DD):")
+        self.entry_id_reserva = self.create_entry(self.frame_medio, "ID de reserva:")
+        self.entry_id_cliente = self.create_entry(self.frame_medio, "ID de cliente:")
+        self.entry_nueva_fecha_inicio = self.create_entry(self.frame_medio, "Nueva fecha de inicio (YYYY-MM-DD):")
+        self.entry_nueva_fecha_fin = self.create_entry(self.frame_medio, "Nueva fecha de fin (YYYY-MM-DD):")
 
-        self.entry_id_cliente_cliente = self.create_entry(self.ventana, "ID del cliente:")
-        self.entry_nombre_cliente = self.create_entry(self.ventana, "Nombre del cliente:")
-        self.entry_contacto_cliente = self.create_entry(self.ventana, "Contacto del cliente:")
-        self.entry_direccion_cliente = self.create_entry(self.ventana, "Dirección del cliente:")
+        self.entry_id_cliente_cliente = self.create_entry(self.frame_derecho, "ID del cliente:")
+        self.entry_nombre_cliente = self.create_entry(self.frame_derecho, "Nombre del cliente:")
+        self.entry_contacto_cliente = self.create_entry(self.frame_derecho, "Contacto del cliente:")
+        self.entry_direccion_cliente = self.create_entry(self.frame_derecho, "Dirección del cliente:")
 
-        self.button_registrar_cliente = tk.Button(self.ventana, text="Registrar Cliente", command=self.registrar_cliente)
-        self.button_registrar_cliente.pack()
+    # BOTONES
+        self.button_registrar_cliente = tk.Button(self.frame_derecho, text="Registrar Cliente",font=('Arial',12), command=self.registrar_cliente,bg="#4EAA03",fg="white")
+        self.button_registrar_cliente.pack(pady=5)
 
-        self.button_modificar_cliente = tk.Button(self.ventana, text="Modificar Cliente", command=self.modificar_cliente)
-        self.button_modificar_cliente.pack()
+        self.button_modificar_cliente = tk.Button(self.frame_derecho, text="Modificar Cliente", font=('Arial',12),command=self.modificar_cliente,bg="#0077b6",fg="white")
+        self.button_modificar_cliente.pack(pady=5)
 
+        self.button_registrar_habitacion = tk.Button(self.frame_izquierdo, text="Registrar Habitación",font=('Arial',12), command=self.registrar_habitacion,bg="#4EAA03",fg="white")
+        self.button_registrar_habitacion.pack(pady=5)
 
-        self.button_registrar_habitacion = tk.Button(self.ventana, text="Registrar Habitación", command=self.registrar_habitacion)
-        self.button_registrar_habitacion.pack()
+        self.button_modificar_habitacion = tk.Button(self.frame_izquierdo, text="Modificar Habitación",font=('Arial',12),command=self.modificar_habitacion,bg="#0077b6",fg="white")
+        self.button_modificar_habitacion.pack(pady=5)
 
-        self.button_modificar_habitacion = tk.Button(self.ventana, text="Modificar Habitación", command=self.modificar_habitacion)
-        self.button_modificar_habitacion.pack()
+        self.button_registrar_reserva = tk.Button(self.frame_medio, text="Registrar Reserva",font=('Arial',12),command=self.registrar_reserva,bg="#4EAA03",fg="white")
+        self.button_registrar_reserva.pack(pady=5)
 
-        self.button_registrar_reserva = tk.Button(self.ventana, text="Registrar Reserva", command=self.registrar_reserva)
-        self.button_registrar_reserva.pack()
+        self.button_modificar_reserva = tk.Button(self.frame_medio, text="Modificar Reserva",font=('Arial',12), command=self.modificar_reserva,bg="#0077b6",fg="white")
+        self.button_modificar_reserva.pack(pady=5)
 
-        self.button_modificar_reserva = tk.Button(self.ventana, text="Modificar Reserva", command=self.modificar_reserva)
-        self.button_modificar_reserva.pack()
+        self.button_cancelar_reserva = tk.Button(self.frame_medio, text="Cancelar Reserva",font=('Arial',12), command=self.cancelar_reserva,bg="#FF4040",fg="white")
+        self.button_cancelar_reserva.pack(pady=5)
 
-        self.button_cancelar_reserva = tk.Button(self.ventana, text="Cancelar Reserva", command=self.cancelar_reserva)
-        self.button_cancelar_reserva.pack()
+    # BOTONES ESTADISTICAS
+        self.button_reporte_ocupacion = tk.Button(self.frame_bajo, text="Reporte de Ocupación",font=('Arial',12), command=self.reporte_ocupacion,bg="#ECAB0F",fg="white")
+        self.button_reporte_ocupacion.pack(pady=5)
 
-        self.button_reporte_ocupacion = tk.Button(self.ventana, text="Reporte de Ocupación", command=self.reporte_ocupacion)
-        self.button_reporte_ocupacion.pack()
+        self.button_reporte_ingresos = tk.Button(self.frame_bajo, text="Reporte Ingresos",font=('Arial',12),command=self.reporte_ingresos,bg="#F7D547",fg="black")
+        self.button_reporte_ingresos.pack(pady=5)
+
+        self.button_analisis_demanda = tk.Button(self.frame_bajo, text="Analisis Demanda",font=('Arial',12),command=self.analisis_demanda,bg="#FFFF72",fg="black")
+        self.button_analisis_demanda.pack(pady=5)
 
         tk.mainloop()
 
     def create_entry(self, ventana, label_text):
-        label = tk.Label(ventana, text=label_text)
+        label = tk.Label(ventana, text=label_text,font=('Arial',12),bg="#D0EDFC",fg="black")
         label.pack()
         entry = tk.Entry(ventana)
         entry.pack()
@@ -211,7 +256,7 @@ class HotelApp:
             self.hotel.registrar_habitacion(id_habitacion, tipo, capacidad, precio, disponible)
             messagebox.showinfo("Éxito", "Habitación registrada correctamente")
         except ValueError:
-            messagebox.showerror("Error", "Por favor, ingrese valores válidos.")
+            messagebox.showerror("Error", "Por favor, ingrese algun valor.")
 
     def modificar_habitacion(self):
         try:
@@ -222,7 +267,7 @@ class HotelApp:
             self.hotel.modificar_habitacion(id_habitacion, nuevo_precio, nueva_capacidad, nueva_disponibilidad)
             messagebox.showinfo("Éxito", "Habitación modificada correctamente")
         except ValueError:
-            messagebox.showerror("Error", "Por favor, ingrese valores válidos.")
+            messagebox.showerror("Error", "Por favor, ingrese algun valor.")
 
     def registrar_reserva(self):
         try:
@@ -236,7 +281,7 @@ class HotelApp:
             self.hotel.registrar_reserva(id_reserva, id_cliente, id_habitacion, fecha_inicio, fecha_fin)
             messagebox.showinfo("Éxito", "Reserva registrada correctamente")
         except ValueError:
-            messagebox.showerror("Error", "Por favor, ingrese valores válidos o verifique el formato de la fecha.")
+            messagebox.showerror("Error", "Por favor, ingrese valores para su reserva.")
 
     def modificar_reserva(self):
         try:
@@ -246,7 +291,7 @@ class HotelApp:
             self.hotel.modificar_reserva(id_reserva, nueva_fecha_inicio, nueva_fecha_fin)
             messagebox.showinfo("Éxito", "Reserva modificada correctamente")
         except ValueError:
-            messagebox.showerror("Error", "Por favor, ingrese valores válidos.")
+            messagebox.showerror("Error", "Por favor, ingrese algun valor.")
 
     def cancelar_reserva(self):
         try:
@@ -254,10 +299,23 @@ class HotelApp:
             self.hotel.cancelar_reserva(id_reserva)
             messagebox.showinfo("Éxito", "Reserva cancelada correctamente")
         except ValueError:
-            messagebox.showerror("Error", "Por favor, ingrese un ID de reserva válido.")
+            messagebox.showerror("Error", "Por favor, ingrese un ID de reserva valido.")
 
     def reporte_ocupacion(self):
         self.hotel.reporte_ocupacion()
+
+    def analisis_demanda(self):
+        self.hotel.analisis_demanda()
+
+    def reporte_ingresos(self):
+        try:
+            fecha_inicio = self.entry_nueva_fecha_inicio.get()
+            fecha_fin = self.entry_nueva_fecha_fin.get()
+            datetime.strptime(fecha_inicio, '%Y-%m-%d')
+            datetime.strptime(fecha_fin, '%Y-%m-%d')
+            self.hotel.reporte_ingresos(fecha_inicio, fecha_fin)
+        except ValueError:
+            messagebox.showerror("Error", "Por favor, ingrese fechas en el formato YYYY-MM-DD.")
 
     def registrar_cliente(self):
         try:
@@ -268,7 +326,7 @@ class HotelApp:
             self.hotel.registrar_cliente(id_cliente, nombre, contacto, direccion)
             messagebox.showinfo("Éxito", "Cliente registrado correctamente")
         except ValueError:
-            messagebox.showerror("Error", "Por favor, ingrese valores válidos.")
+            messagebox.showerror("Error", "Por favor, ingrese algun valor.")
 
     def modificar_cliente(self):
         try:
@@ -279,7 +337,6 @@ class HotelApp:
             self.hotel.modificar_cliente(id_cliente, nuevo_nombre, nuevo_contacto, nueva_direccion)
             messagebox.showinfo("Éxito", "Cliente modificado correctamente")
         except ValueError:
-            messagebox.showerror("Error", "Por favor, ingrese valores válidos.")
-
+            messagebox.showerror("Error", "Por favor, ingrese algun valor.")
 
 app = HotelApp()
